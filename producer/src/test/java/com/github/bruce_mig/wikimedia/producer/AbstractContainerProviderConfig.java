c0a7c8000e412373ec6ff182e341c36ed795c045
bruce-mig <brucetmig@icloud.com>
2025-04-10 11:33:07 +0200

    added producer E2E test

--- producer/src/test/java/com/github/bruce_mig/wikimedia/producer/AbstractContainerProviderConfig.java
+++ producer/src/test/java/com/github/bruce_mig/wikimedia/producer/AbstractContainerProviderConfig.java
@@ -3,6 +3,7 @@ package com.github.bruce_mig.wikimedia.producer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.lifecycle.Startables;
@@ -12,7 +13,7 @@ import org.testcontainers.utility.DockerImageName;
 @Testcontainers
 public class AbstractContainerProviderConfig {
 
-//    static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"));
+    @Container
     static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.9.0")
         .asCompatibleSubstituteFor("apache/kafka"));
 
